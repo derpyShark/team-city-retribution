e2677f138c83b4f69d6bba18b6b76d03b3815dfb
Denys Kudriev <denys_kudriev@epam.com>
2021-04-19 18:05:37 +0300

    Initial commit

---
+++ Lib/SuperDuperMath.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Lib
+{
+    public class SuperDuperMath
+    {
+        public static int MultiplyByTwo(int input) => input * 2;
+    }
+}
---
+++ Tests/SuperMathTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using Lib;
+
+namespace Tests
+{
+    public class SuperMathTests
+    {
+        [Test]
+        public static void SuperTest()
+        {
+            var result = SuperDuperMath.MultiplyByTwo(2);
+            Assert.AreEqual(4, result, "Failed to multiply by two");
+        }
+    }
+}
